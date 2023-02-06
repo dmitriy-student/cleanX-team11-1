a694207383331152a7285cae8a953c006bf0b382
dmitriy-student <116590602+dmitriy-student@users.noreply.github.com>
2023-02-06 18:15:19 +0000

    Deploying to gh-pages from @ dmitriy-student/cleanX-team11-1@30bcbd95494f79d93fc1e8dc129561331ce0bde6 🚀

---
+++ index.63367091.js
@@ -0,0 +1,2 @@
+$(".responsive").slick({prevArrow:'<button type="button" class="my-slick-prev"></button>',nextArrow:'<button type="button" class="my-slick-next"></button>',infinite:!0,speed:300,slidesToScroll:1,adaptiveHeight:!0,slidesToShow:2,responsive:[]});
+//# sourceMappingURL=index.63367091.js.map
