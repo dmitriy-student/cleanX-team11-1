1ab4009ce4ebfb92d91396b85dd3d04b3698d778
CaXaR777 <116805047+CaXaR777@users.noreply.github.com>
2023-02-06 22:27:21 +0000

    Deploying to gh-pages from @ dmitriy-student/cleanX-team11-1@0fdae0548ccc1beea2db9da88bf78bdccf19c5ae 🚀

---
+++ index.15538637.js
@@ -0,0 +1,2 @@
+$(".autoplay").slick({infinite:!0,speed:300,slidesToScroll:1,adaptiveHeight:!0,slidesToShow:2,autoplaySpeed:3e3,autoplay:!0,arrows:!1,responsive:[]});
+//# sourceMappingURL=index.15538637.js.map
--- index.b2c6df73.js
+++
@@ -1,2 +0,0 @@
-$(".responsive").slick({prevArrow:'<button type="button" class="my-slick-prev"></button>',nextArrow:'<button type="button" class="my-slick-next"></button>',infinite:!0,speed:300,slidesToScroll:1,adaptiveHeight:!0,slidesToShow:2,responsive:[]});
-//# sourceMappingURL=index.b2c6df73.js.map
